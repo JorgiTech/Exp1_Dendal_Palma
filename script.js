ec7cfc67fb4a382826de3e48514950f403a7b365
nico <nicol@LAPTOP-7MDKU7Q2>
2023-05-17 01:03:42 -0400

    "Arreglo" de alertas

--- script.js
+++ script.js
@@ -11,35 +11,74 @@ function iniciarMap() {
 
     });
 }
-//var nombre = document.getElementById('nombres');
-//var email = document.getElementById('correo_electronico');
-//var error = document.getElementById('error');
-//error.style.color = 'green';
-//function enviarFormulario(){
-//    console.log('Enviando formulario...');
 
-//    var mensasjeError = [];
-
-//    if(nombre.value === null || nombre.value === ''){
-//        mensajesError.push('Ingresa tu nombre');
-//    }
-
-//    error.innerHTML = mensasjeError.join(', ');
-
+//var form = document.getElementById('form');
+//    form.addEventListener('submit', function(event){
+//        event.preventDefault();
+//        var mensajeError = [];
+//
+//        if(nombres.value.length <6 || null || nombres.value === '' ){
+//            mensajeError.push('El nombre no es valido <br>');
+//        }
+//       if(apellido_paterno.value.length <6 || null || apellido_paterno.value === '' ){
+//            mensajeError.push('El apellido paterno no es valido <br>');
+//        }
+//        if(apellido_materno.value.length <6 || null || apellido_materno.value === '' ){
+//            mensajeError.push('El apellido materno no es valido <br>');
+//        }
+//        if(rut.value.length <6 || null || rut.value === '' ){
+//            mensajeError.push('El rut no es valido <br>');
+//        }
+//        if(direccion.value.length <6 || null || direccion.value === '' ){
+//            mensajeError.push('La dirección no es valida <br>');
+//        }
+//        if(correo_electronico.value.length <6 || null || correo_electronico.value === '' ){
+//            mensajeError.push('El correo electrónico no es valido <br>');
+//        }
+//        if(telefono.value.length <6 || null || telefono.value === '' ){
+//            mensajeError.push('El telefono no es valido <br>');
+//        }
+//
+//    error.innerHTML = mensajeError.join(' ');
+//
 //    return false;
-//}
+//});
 
 
-var form = document.getElementById('form');
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
-        var mensajeError = [];
 
-        if(nombres.value.length <6 || null || nombres.value === '' ){
-            mensajeError.push('Ingresa tu nombre');
-        }
+const nombre = document.getElementById("nombres")
+const apellidoP = document.getElementById("apellido_paterno")
+const apellidoM = document.getElementById("apellido_materno")
+const rut = document.getElementById("rut")
+const dir = document.getElementById("direccion")
+const email = document.getElementById("correo_electronico")
+const tel = document.getElementById("telefono")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-    error.innerHTML = mensajeError.join(', ');
-
-    return false;
-});
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let entrar = false
+    let regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if(nombre.value.length <6){
+        alert("El Nombre no es valido.")
+    }
+    if(apellidoP.value.length <6){
+        alert("El Apellido Paterno no es valido.")
+    }
+    if(apellidoM.value.length <6){
+        alert("El Apellido Materno no es valido.")
+    }
+    if(rut.value.length <10){
+        alert("El rut no es valido.")
+    }
+    if(dir.value.length <6){
+        alert("La Dirección no es valida.")
+    }
+    if(!regexemail.test(email.value)) {
+        alert("El Correo Electronico no es valido.")
+    }
+    if(tel.value.length <6){
+        alert("El Teléfono no es valido.")
+    }
+})
