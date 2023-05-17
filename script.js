e9f6e97f1509960594cde857c5dac4124de920bb
nico <nicol@LAPTOP-7MDKU7Q2>
2023-05-17 00:04:01 -0400

    sus

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function iniciarMap() {
-    var coord = { lat: -33.5113338,lng: -70.7525613 };
+    var coord = { lat: -33.539009,lng: -70.7728858 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: coord
@@ -11,3 +11,35 @@ function iniciarMap() {
 
     });
 }
+//var nombre = document.getElementById('nombres');
+//var email = document.getElementById('correo_electronico');
+//var error = document.getElementById('error');
+//error.style.color = 'green';
+//function enviarFormulario(){
+//    console.log('Enviando formulario...');
+
+//    var mensasjeError = [];
+
+//    if(nombre.value === null || nombre.value === ''){
+//        mensajesError.push('Ingresa tu nombre');
+//    }
+
+//    error.innerHTML = mensasjeError.join(', ');
+
+//    return false;
+//}
+
+
+var form = document.getElementById('form');
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        var mensajeError = [];
+
+        if(nombres.value.length <6 || null || nombres.value === '' ){
+            mensajeError.push('Ingresa tu nombre');
+        }
+
+    error.innerHTML = mensajeError.join(', ');
+
+    return false;
+});
