02b7271c3806f91564d354cbda68736755e7c429
jorgi <Jo.palmas@duocuc.cl>
2023-07-09 18:33:40 -0400

    ajustes varios

--- autotech/app/urls.py
+++ autotech/app/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from .views import index,nosotros,galeria,api,formulario,productos
+from .views import index,nosotros,galeria,api,formulario
+from .import views
 
 
 urlpatterns = [
+    path('', views.index, name='home'), 
     path('index/', index,name='index'),
     path('nosotros/', nosotros,name='nosotros'),
     path('galeria/', galeria,name='galeria'),
     path('api/', api,name='api'),
     path('formulario/', formulario,name='formulario'),
-    path('productos/', productos,name='productos')
+    path('productos/', views.productos, name='productos'),
+    path('agregar/', views.agregar, name='agregar'),
+    path('agregarsave', views.agregarsave, name='agregarsave'),
+    path('productos/eliminar/<int:id>/', views.eliminar, name='eliminar'),
+
+ #   path('eliminar/<int:id>/', views.eliminar, name='eliminar'),
+    path('productos/actualizar/<int:id>/', views.actualizar, name='actualizar'),
+    path('actualizarsave/<int:id>/', views.actualizarsave, name='actualizarsave')
+
 ]
--- autotech/app/views.py
+++ autotech/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
 from .models import Producto
 
 
@@ -14,10 +14,49 @@ def galeria(request):
 
 def api(request):
     return render(request,'app/api.html')
-
+#agregar xD
 def formulario(request):
     return render(request,'app/formulario.html')
 
 def productos(request):
     pro=Producto.objects.all()
     return render(request,'app/productos.html',{'pro':pro})
+
+def agregar(request):
+    return render(request,'app/agregar.html')
+
+from django.shortcuts import redirect
+
+def agregarsave(request):
+    if 'nombre' in request.POST:
+        x = request.POST['id']
+        y = request.POST['nombre']
+        z = request.POST['valor']
+        pro = Producto(id=x, nombre=y, valor=z)
+        pro.save()
+        return redirect("/productos/")
+    else:
+        return redirect("/productos/")
+
+
+
+def eliminar(request, id):
+    pro=Producto.objects.get(id=id)
+    pro.delete()
+    return redirect("/productos/")
+
+
+def actualizar(request,id):
+    pro=Producto.objects.get(id=id)
+    return render(request, 'actualizar.html',{'pro':pro})
+
+def actualizarsave(request,id):
+    x=request.POST['id']
+    y=request.POST['nombre']
+    z=request.POST['valor']
+    pro=Producto.objects.get(id=id)
+    pro.id=x
+    pro.nombre=y
+    pro.valor=z
+    pro.save()
+    return redirect("/productos/")
